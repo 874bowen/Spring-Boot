1a8e0c7c7dba7ebbfbd282a97b60ab556d972f73
bowen <bowenivan16@gmail.com>
2022-05-29 09:22:23 +0300

    springboot is for automatic configuration makes the hard job for Spring Standalone easier

--- src/main/java/com/springboot/intro/springboot/SpringbootApplication.java
+++ src/main/java/com/springboot/intro/springboot/SpringbootApplication.java
@@ -1,13 +1,29 @@
 package com.springboot.intro.springboot;
 
+import org.apache.catalina.core.ApplicationContext;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
+/* @SpringBootApplication ->
+1. indicates that this is a Spring context class
+2. enables AutoConfiguration
+3. Enables ComponentScan intro & springboot for any beans BookController would be registered as a bean and would be managed by the
+SpringFramework
+SpringBoot looks at (a) Frameworks available on the CLASSPATH (b) Existing configuration for the application. Based on these,
+SpringBoot provides basic configuration needed to configure the application with these frameworks . This is called
+AUTO CONFIGURATION
+*  */
 @SpringBootApplication
 public class SpringbootApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringbootApplication.class, args);
+		// run method used to run spring context and returns an applicationContext
+		ConfigurableApplicationContext applicationContext = SpringApplication.run(SpringbootApplication.class, args);
+
+		for (String name : applicationContext.getBeanDefinitionNames()){
+			System.out.println(name);
+		}
 	}
 
 }
