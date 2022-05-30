1f3b8767cf08b1c0df13fa3fa3dec50aeb1962d2
bowen <bowenivan16@gmail.com>
2022-05-30 07:52:25 +0300

    implemented a commandlineRunner and autowired our dependencies and called them in the run method

--- src/main/java/com/springboot/intro/springboot/SpringbootApplication.java
+++ src/main/java/com/springboot/intro/springboot/SpringbootApplication.java
@@ -1,6 +1,12 @@
 package com.springboot.intro.springboot;
 
+import com.springboot.intro.springboot.aop.business.Business1;
+import com.springboot.intro.springboot.aop.business.Business2;
 import org.apache.catalina.core.ApplicationContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
@@ -14,16 +20,31 @@ SpringBoot looks at (a) Frameworks available on the CLASSPATH (b) Existing confi
 SpringBoot provides basic configuration needed to configure the application with these frameworks . This is called
 AUTO CONFIGURATION
 *  */
-@SpringBootApplication
-public class SpringbootApplication {
 
+// implemented a commandlineRunner and autowired our dependencies and called them in the run method
+@SpringBootApplication
+public class SpringbootApplication implements CommandLineRunner {
+	// use LOGGER
+	private Logger logger = LoggerFactory.getLogger(this.getClass());
+	// good thing now we can autowire stuff and configure them outside the static method
+	@Autowired
+	private Business1 business1;
+
+	@Autowired
+	private Business2 business2;
 	public static void main(String[] args) {
 		// run method used to run spring context and returns an applicationContext
-		ConfigurableApplicationContext applicationContext = SpringApplication.run(SpringbootApplication.class, args);
+		SpringApplication.run(SpringbootApplication.class, args);
 
-		for (String name : applicationContext.getBeanDefinitionNames()){
-			System.out.println(name);
-		}
+//		for (String name : applicationContext.getBeanDefinitionNames()){
+//			System.out.println(name);
+//		}
 	}
-
+	// run method is defined in the commandlinerunner
+	// whenever the springbootapplication launches up it also calls the run method
+	@Override
+	public void run(String... args) throws Exception {
+		logger.info(business1.calculateSomething());
+		logger.info(business2.calculateSomething());
+	}
 }
